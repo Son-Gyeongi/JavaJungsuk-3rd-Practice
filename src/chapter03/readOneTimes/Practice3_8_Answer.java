652ba3fa9e9e92957e9665de1f1ebed17aed928d
Son-Gyeongi <thsruddl77@gmail.com>
2025-04-02 18:02:09 +0900

    chapter03 Practice Answer 1st

---
+++ src/chapter03/readOneTimes/Practice3_8_Answer.java
@@ -0,0 +1,26 @@
+package chapter03.readOneTimes;
+
+public class Practice3_8_Answer {
+    public static void main(String[] args) {
+        byte a = 10;
+        byte b = 20;
+        byte c = (byte) (a + b);
+
+        char ch = 'A';
+        ch = (char) (ch + 2);
+
+        float f = 3f / 2;
+        long l = 3000L * 3000 * 3000;
+
+        float f2 = 0.1f;
+        double d = 0.1;
+
+        boolean result = (float) d == f2;
+
+        System.out.println("c = " + c);
+        System.out.println("ch = " + ch);
+        System.out.println("f = " + f);
+        System.out.println("l = " + l);
+        System.out.println("result = " + result);
+    }
+}
